f18c3a6f6af0409943cbfeb00888df4a79791ffc
YpsilonTM <22861150+YpsilonTM@users.noreply.github.com>
2024-02-23 11:23:17 +0100

    Update tailwind.config.js with new theme options

--- svelte/tailwind.config.js
+++ svelte/tailwind.config.js
@@ -1,25 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {},
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: [
-      {
-        worldz: {
-          primary: '#91c745',
-          secondary: '#6e38bb',
-          accent: '#6e8b44',
-          neutral: '#6b7280',
-          'base-100': '#1f2937',
-          'base-200': '#111827',
-          'base-300': '#030712',
-          info: '#5597ce',
-          success: '#26c063',
-          warning: '#d2b214',
-          error: '#e55238',
-        },
-      },
-    ],
+    themes: ['dark'],
   },
 }
