3d06c3751fb76f5fea6d775d63986465ee5fd1ad
YpsilonTM <22861150+YpsilonTM@users.noreply.github.com>
2024-04-03 16:13:40 +0200

    Add netPrefix to client.lua

--- client.lua
+++ client.lua
@@ -1,11 +1,25 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local netPrefix = GetCurrentResourceName() .. ':'
 
 function PassToServer(name)
     RegisterNUICallback(name, function(payload, cb)
-        QBCore.Functions.TriggerCallback(GetCurrentResourceName() .. ':' .. name, function(result)
-            cb(result)
+        lib.callback(netPrefix .. name, false, function(data)
+            cb(data)
         end, payload)
     end)
 end
 
 PassToServer('Example')
+
+RegisterNetEvent(netPrefix .. 'toggleUI', function(bool)
+    SetNuiFocus(bool, bool)
+    SendNUIMessage({
+        action = 'show',
+        data = bool
+    })
+end)
+
+RegisterNUICallback('close', function(payload, cb)
+    TriggerEvent(netPrefix .. 'toggleUI', false)
+    cb('ok')
+end)
